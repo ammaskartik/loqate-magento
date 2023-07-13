36835fc2b93ee38e2e58ab4db2609667fbfbaced
georgeirimiciuc <george.irimiciuc@endava.com>
2023-07-13 22:16:23 +0300

    GBGAP001-92 take config into value

--- Plugin/ChangeCheckoutDefaultCountry.php
+++ Plugin/ChangeCheckoutDefaultCountry.php
@@ -2,6 +2,7 @@
 
 namespace Loqate\ApiIntegration\Plugin;
 
+use Loqate\ApiIntegration\Helper\Data;
 use Loqate\ApiIntegration\Helper\Extra;
 use Magento\Checkout\Block\Checkout\LayoutProcessorInterface;
 use Magento\Directory\Model\CountryFactory;
@@ -10,17 +11,23 @@ class ChangeCheckoutDefaultCountry
 {
     protected $countryFactory;
     private Extra $extra;
+    private Data $helper;
 
-    public function __construct(CountryFactory $countryFactory, Extra $extra)
+    public function __construct(CountryFactory $countryFactory, Extra $extra, Data $helper)
     {
         $this->countryFactory = $countryFactory;
         $this->extra = $extra;
+        $this->helper = $helper;
     }
 
     public function afterProcess(
         LayoutProcessorInterface $subject,
         array $jsLayout
     ) {
+        if (!$this->helper->getConfigValue('loqate_settings/ipcountry_settings/enable_checkout')) {
+            return $jsLayout;
+        }
+
         $countryResult = $this->extra->ipToCountry();
 
         if (isset($countryResult['Iso2']) && $countryResult['Iso2'] != null) {
